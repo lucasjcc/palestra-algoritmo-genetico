073b3f9f438b9e12071252a883af7fe17258ce80
Lucas Carvalho <lucas.carvalho.ext@spms.min-saude.pt>
2026-05-27 12:25:29 -0300

    adicionado o carregamento da base de dados

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { letDataset } from "./auxiliares/letDataset.js";
 
-let tamanhoPopulacao = 50;
-
+// Carregamento da base de dados
 const caminhoRelativoDataset = "datasets/br17.txt";
 const dimensaoDataset = 17;
 const matrizDistancias = letDataset(caminhoRelativoDataset, dimensaoDataset);
-console.log(matrizDistancias);
