a23644b3705e5c0a0c0146f9f1b871d620d45511
Lucas Carvalho <lucas.carvalho.ext@spms.min-saude.pt>
2026-06-01 17:28:55 -0300

    correção da função de gerar número aleatório para incluir limite superior

--- auxiliares/gerarNumeroAleatorioInteiroEntre.js
+++ auxiliares/gerarNumeroAleatorioInteiroEntre.js
@@ -1,5 +1,5 @@
 export function gerarNumeroAleatorioInteiroEntre(limiteInferior, limiteSuperior) {
     const aleatorio = Math.random();
-    const numeroAleatorio = limiteInferior + aleatorio * (limiteSuperior - limiteInferior);
+    const numeroAleatorio = limiteInferior + aleatorio * (limiteSuperior - limiteInferior + 1);
     return Math.floor(numeroAleatorio);
 }
